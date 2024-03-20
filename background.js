261026a9906fc5d9999c5a2ea943dc1f1712c4cc
Bluefrog922 <140343859+Bluefrog922@users.noreply.github.com>
2024-03-20 07:28:15 -0700

    v1.1

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
 chrome.tabs.onActivated.addListener(({ tabId }) => {
   chrome.tabs.get(tabId, (tab) => {
-    if (tab.url.startsWith("about:blank")) {
-      chrome.tabs.update(tabId, { url: "about:blank" });
+    if (tab.url.startsWith("about:blank*")) {
+      chrome.tabs.update(tabId, { url: "blank.html" });
     }
   });
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.url && changeInfo.url.startsWith("about:blank")) {
-    chrome.tabs.update(tabId, { url: "about:blank" });
+  if (changeInfo.url && changeInfo.url.startsWith("about:blank*")) {
+    chrome.tabs.update(tabId, { url: "blank.html" });
   }
 });
